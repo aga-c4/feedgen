74f9ef18d69f367427cd852c8a996460b9d7adda
Konstantin Khachaturyan <aga-c4@rambler.ru>
2020-08-14 13:40:53 +0300

    constants update

--- config/constants.php
+++ config/constants.php
@@ -10,10 +10,10 @@ if (!defined("InStock_Regional_0")) define('InStock_Regional_0',512); //- тов
 if (!defined("InStock_Shop_3")) define('InStock_Shop_3',256); //- наличие на витрине в региональне
 if (!defined("InStock_Shop_2")) define('InStock_Shop_2',128); //- наличие на витрине в региональне
 if (!defined("InStock_Shop_1")) define('InStock_Shop_1',64); //- ограниченное количество на витрине в регионе
-if (!defined("InStock_Donor_3")) define('InStock_Donor_3',32); //- наличие в Москве на складе более 2х
-if (!defined("InStock_Donor_2")) define('InStock_Donor_2',16); //- наличие в Москве на складе 2шт
-if (!defined("InStock_Donor_1")) define('InStock_Donor_1',8); //- ограниченное количество в Москве на складе 1шт.
-if (!defined("InStock_Donor_0")) define('InStock_Donor_0',4); //- товар в пути в Москве
+if (!defined("InStock_Donor_3")) define('InStock_Donor_3',32); //- наличие на базовом складе более 2х
+if (!defined("InStock_Donor_2")) define('InStock_Donor_2',16); //- наличие на базовом складе 2шт
+if (!defined("InStock_Donor_1")) define('InStock_Donor_1',8); //- ограниченное количество на базовом складе 1шт.
+if (!defined("InStock_Donor_0")) define('InStock_Donor_0',4); //- товар в пути на базовом складе
 if (!defined("InStock_Dealers")) define('InStock_Dealers',2); //- наличие товара у региональных поставщиков
 if (!defined("InStock_Production")) define('InStock_Production',1); //- можно заказать производство
 if (!defined("InStock_Empty")) define('InStock_Empty',0); //- нет наличия
